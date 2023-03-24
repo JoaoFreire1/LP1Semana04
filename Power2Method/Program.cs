889954715b3b6d1c4c83e7b3feadfbbb162536a3
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-24 10:46:18 +0000

    Exercise 04, Method add

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -7,9 +7,9 @@ namespace Power2Method
     class Program
     {
         
-        private static void Powersof2Until5()
+        private static void Powersof2Untiln(int n)
         {
-        for (int i = 1; i <= (1<<5); i = i << 1)
+        for (int i = 1; i <= (1<<n); i = i << 1)
         {
             Console.WriteLine(i);
         }
