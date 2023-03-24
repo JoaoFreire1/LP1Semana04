004dcbb0788837edd82375be4a928f4d21e12707
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-24 09:36:03 +0000

    Exercise 01, basis

---
+++ Power2Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Power2Method
+{
+    private static void Powersof2Until5()
+    {
+        for (int i = 1; i <= (1<<5); i = i << 1)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
