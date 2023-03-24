bb8d64f3d4b07e96fb24307dc23bbae4ceaa7bf8
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-24 09:57:22 +0000

    Exercisoe 01, complete

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -2,20 +2,24 @@
 
 namespace Power2Method
 {
-    private static void Powersof2Until5()
+    
+
+    class Program
     {
+        
+        private static void Powersof2Until5()
+        {
         for (int i = 1; i <= (1<<5); i = i << 1)
         {
             Console.WriteLine(i);
         }
 
-    }
-
-    class Program
-    {
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Powersof2Until5();
+            Powersof2Until5();
         }
     }
 }
