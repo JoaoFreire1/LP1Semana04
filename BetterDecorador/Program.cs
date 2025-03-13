f7a92f4a5d72a77b23cc0c47dbed74cf0c65e9e6
JoaoFreire1 <joao.freire.art@gmail.com>
2025-03-13 21:32:55 +0000

    Exercise 02

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -2,14 +2,28 @@
 
 namespace BetterDecorador
 {
+    /// <summary>
+    /// Program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main function, send strings to Decor function
+        /// </summary>
+        /// <param name="args">Console Arguments</param>
         private static void Main(string[] args)
         {
             Console.WriteLine(Decor("Good Morning!" , '*', 3));
             Console.WriteLine(Decor("Good Evening!" , '.', 3));
         }
 
+        /// <summary>
+        /// Decorates sentences
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="dec"></param>
+        /// <param name="num"></param>
+        /// <returns>A string, decorated sentence</returns>
         private static string Decor( string s , char dec, int ntimes)
         {
             string stringdecor = "";
