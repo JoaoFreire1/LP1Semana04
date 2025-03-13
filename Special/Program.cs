491ca6b4a050ab213a5235e60634a03b09f26b31
JoaoFreire1 <joao.freire.art@gmail.com>
2025-03-13 23:17:43 +0000

    Get Specialmethod

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,21 @@ namespace Special
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string sinput = Console.ReadLine();
+            int input = int.Parse(sinput);
+            int result = GetSpecial(input);    
+            Console.WriteLine(result); 
         }
+
+        private static int GetSpecial (int n) 
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+    
+            return GetSpecial(n-1) + (GetSpecial(n-2) * 2);
+        }
+            
     }
 }
